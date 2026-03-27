1d568c2c0b472db2c66964c7ced4ef0807c9825e
Thyruh <mebiysy@gmail.com>
2026-03-27 02:58:31 +0100

    Added a global nob wrapper that compiles a nob.c file if one is present in case a ./nob is not present. Communicates if no trace nob is found.

--- .config/nvim/lua/thyruh/remap.lua
+++ .config/nvim/lua/thyruh/remap.lua
@@ -1,9 +1,14 @@
-vim.g.mapleader = " "  -- Set the leader key to space
+vim.g.mapleader = " "
 
--- A one-back undo for clipboards
 local function yank_both()
-  vim.cmd('normal! "+y')
-  vim.fn.setreg('"', vim.fn.getreg('+'))
+  local mode = vim.fn.mode()
+  if mode == 'v' or mode == 'V' or mode == '\22' then
+    vim.cmd('normal! "+y')
+    vim.fn.setreg('"', vim.fn.getreg('+'))
+  else
+    vim.cmd('normal! "+yy')
+    vim.fn.setreg('"', vim.fn.getreg('+'))
+  end
 end
 
 vim.keymap.set("v", "y", yank_both, { noremap = true, silent = true })
@@ -11,8 +16,6 @@ vim.keymap.set("n", "yy", yank_both, { noremap = true, silent = true })
 
 vim.keymap.set({"n","v"}, "p", '"+p')
 vim.keymap.set({"n","v"}, "<leader>p", '"0p')
-
--- next 
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("Ex")
@@ -32,34 +35,27 @@ vim.keymap.set("i", "<TAB>", function()
   return "<C-x><C-o>"
 end, { expr = true, silent = true })
 
--- pre setup with the types
 vim.keymap.set("n", "<C-f>", "i#include <stdio.h>\n#include <stdbool.h>\n#include <stdint.h>\n\ntypedef int8_t i8;\ntypedef int16_t i16;\ntypedef int32_t i32;\ntypedef int64_t i64;\ntypedef uint8_t u8;\ntypedef uint16_t u16;\ntypedef uint32_t u32;\ntypedef uint64_t u64;\n\ntypedef float f32;\ntypedef double f64;\n\nint main(void) {\nprintf(\"Hello, World!\\n\");\nreturn 0;\n}")
-
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])  -- Yanks the whole line to system clipboard in normal mode
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "E", "$")
 vim.keymap.set("n", "Q", "0")
-vim.keymap.set("n", "yE", "y$")
 vim.keymap.set("n", "dE", "d$")
-
-
--- Open file explorer with <leader>pv
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("Ex")
-end)
+vim.keymap.set("n", "yE", "y$")
 
 vim.keymap.set('n', "<C-d>", '<Nop>')
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])  -- Yanks selected text to system clipboard in normal and visual mode
-vim.keymap.set("n", "<leader>Y", [["+Y]])  -- Yanks the whole line to system clipboard in normal mode
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- === Emacs-inspired window management ===
+vim.keymap.set("n", "<leader>0", function()
+    local cur_win = vim.api.nvim_get_current_win()
+    for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+        if win ~= cur_win then
+            vim.api.nvim_win_close(win, true)
+        end
+    end
+end, { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>0", function()
     local cur_win = vim.api.nvim_get_current_win()
